add0864aaf20e80dfa4b36f898001d862acb452d
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-16 17:50:54 +0100

    Create Samsung_AC_F1F2com.h

---
+++ components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
+
+namespace esphome {
+namespace Samsung_AC_F1F2com {
+
+class Samsung_AC_F1F2com : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
+ public:
+  void setup() override;
+  void update() override;
+  void loop() override;
+  void dump_config() override;
+};
+
+}  // namespace Samsung_AC_F1F2com
+}  // namespace esphome
