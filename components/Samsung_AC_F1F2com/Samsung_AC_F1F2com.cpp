cf645bae3e23d7245fa5d986319b9a639d54972a
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-16 17:49:47 +0100

    Create Samsung_AC_F1F2com.cpp

---
+++ components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.cpp
@@ -0,0 +1,26 @@
+#include "esphome/core/log.h"
+#include "Samsung_AC_F1F2com.h"
+
+namespace esphome {
+namespace Samsung_AC_F1F2com {
+
+static const char *TAG = "Samsung_AC_F1F2com.sensor";
+
+void Samsung_AC_F1F2com::setup() {
+
+}
+
+void Samsung_AC_F1F2com::update() {
+
+}
+
+void Samsung_AC_F1F2com::loop() {
+
+}
+
+void Samsung_AC_F1F2com::dump_config(){
+    ESP_LOGCONFIG(TAG, "Samsung_AC_F1F2com");
+}
+
+}  // namespace Samsung_AC_F1F2com
+}  // namespace esphome
