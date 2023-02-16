266a5bca2038b2714ce0b7421024e863f1d299b0
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-16 18:46:07 +0100

    Update Samsung_AC_F1F2com.cpp

--- components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.cpp
+++ components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.cpp
@@ -15,11 +15,41 @@ void Samsung_AC_F1F2com::update() {
 }
 
 void Samsung_AC_F1F2com::loop() {
+  const uint32_t now = millis();
+  if (receiving_ && (now - last_transmission_ >= 500)) {
+    // last transmission too long ago. Reset RX index.
+    ESP_LOGW(TAG, "Last transmission too long ago. Reset RX index.");
+    data_.clear();
+    receiving_ = false;
+  }
+    
+  if (!available())
+    // nothing in uart-input-buffer, end here
+    return;
+  
+  last_transmission_ = now;
+  while (available()) {
+    uint8_t c;
+    read_byte(&c);
+    if (!receiving_) {
+      if (c != 0xaa)    //Wenn nicht End-Marker empfangen (0x34) dann weiter empfangen
+        continue;
+      receiving_ = true;
+      continue;
+    }
+    data_.push_back(c);
+    if (data_.size == 14) {
+      if (check_data_())
+        parse_data_();
+      data_.clear();
+      receiving_ = false;
+    }
+  }
 
 }
 
 void Samsung_AC_F1F2com::dump_config(){
-    ESP_LOGCONFIG(TAG, "Samsung_AC_F1F2com");
+    ESP_LOGCONFIG(TAG, "Samsung_AC_F1F2com:");
 }
 
 }  // namespace Samsung_AC_F1F2com
