c7b83b4023de398608fb82f2642881bfecb881a1
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-24 17:33:25 +0100

    Update Samsung_AC_F1F2com.h

--- components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.h
+++ components/Samsung_AC_F1F2com/Samsung_AC_F1F2com.h
@@ -9,6 +9,8 @@ namespace Samsung_AC_F1F2com {
 
 class Samsung_AC_F1F2com : public PollingComponent, public uart::UARTDevice {
  public:
+  void set_room_temp_sensor(sensor::Sensor *room_temp_sensor) { room_temp_sensor_ = room_temp_sensor; }
+ 
   void setup() override;
   void update() override;
   void loop() override;
@@ -17,8 +19,10 @@ class Samsung_AC_F1F2com : public PollingComponent, public uart::UARTDevice {
  protected:
   boolean check_data_() const;
   void parse_data_();
-  std::vector<uint8_t> data_;
  
+ sensor::Sensor *room_temp_sensor_{nullptr};
+ 
+  std::vector<uint8_t> data_;
   bool receiving_{false};
   uint8_t data_count_;
   uint32_t last_transmission_{0};
