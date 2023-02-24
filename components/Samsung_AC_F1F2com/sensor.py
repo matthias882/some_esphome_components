b6684d1d09e68d9a3f50389597965f51c180d657
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-24 17:51:06 +0100

    Update sensor.py

--- components/Samsung_AC_F1F2com/sensor.py
+++ components/Samsung_AC_F1F2com/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
+from esphome.const import CONF_ID, ICON_EMPTY, ICON_THERMOMETER, UNIT_EMPTY, UNIT_CELSIUS
 
 DEPENDENCIES = ['uart']
 
