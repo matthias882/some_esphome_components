18ecc105cebc4b3bc01d62e5d14366eeaa97c0a5
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-24 18:05:56 +0100

    Update sensor.py

--- components/Samsung_AC_F1F2com/sensor.py
+++ components/Samsung_AC_F1F2com/sensor.py
@@ -18,7 +18,6 @@ CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
     await uart.register_uart_device(var, config)
     
     if CONF_ROOM_TEMP_1 in config:
