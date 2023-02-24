25ee7285e242209b9cd889979607f02e6e90fc4f
matthias882 <30553262+matthias882@users.noreply.github.com>
2023-02-24 17:30:36 +0100

    Update sensor.py

--- components/Samsung_AC_F1F2com/sensor.py
+++ components/Samsung_AC_F1F2com/sensor.py
@@ -5,12 +5,15 @@ from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ['uart']
 
-empty_uart_sensor_ns = cg.esphome_ns.namespace('empty_uart_sensor')
-EmptyUARTSensor = empty_uart_sensor_ns.class_('EmptyUARTSensor', cg.PollingComponent, uart.UARTDevice)
+Samsung_AC_F1F2com_ns = cg.esphome_ns.namespace('Samsung_AC_F1F2com')
+Samsung_AC_F1F2comComponent = Samsung_AC_F1F2com_ns.class_('Samsung_AC_F1F2comComponent', cg.PollingComponent, uart.UARTDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
-    cv.GenerateID(): cv.declare_id(EmptyUARTSensor),
-}).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
+CONF_ROOM_TEMP_1 = "room_temp"
+
+CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(Samsung_AC_F1F2comComponent),
+    cv.Optional(CONF_ROOM_TEMP_1): sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 0),
+}).extend(cv.polling_component_schema('60s'))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -18,3 +21,7 @@ def to_code(config):
     yield sensor.register_sensor(var, config)
     yield uart.register_uart_device(var, config)
     
+    if CONF_ROOM_TEMP_1 in config:
+        sens = await sensor.new_sensor(config[CONF_ROOM_TEMP_1])
+        cg.add(var.set_room_temp_sensor(sens))
+
